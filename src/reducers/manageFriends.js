f4e9e04850e736dfa8b8a89f8f829185958adf63
Tonyjlum <tonyjlum@gmail.com>
2019-02-16 22:32:01 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,12 +1,11 @@
 export function manageFriends(state, action){
   if (action.type === "ADD_FRIEND"){
-    return ({...state, friends: [...state.friends, action.friend]})
+    return ({ friends: [...state.friends, action.friend]})
   } else if (action.type === 'REMOVE_FRIEND'){
     const newlist = state.friends.filter( friend => {
-      console.log(friend.id, "state")
       return friend.id !== action.id
     })
-    return {...state, friends: newlist}
+    return {friends: newlist}
   } else {
     return state
   }
