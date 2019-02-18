7dfc41299eae7d97e74b3ad80bec6780bcdf739a
Damon Collins <damon.c.p.collins@gmail.com>
2019-02-18 12:26:23 -0500

    done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+
+export function manageFriends(state = {friends: []}, action){
+  if (action.type === "ADD_FRIEND"){
+    return {...state, friends: [...state.friends, action.friend]}
+  } else if (action.type === "REMOVE_FRIEND"){
+    const newFriends = [...state.friends].filter(friend => friend.id !== action.id)
+    return {friends: newFriends}
+  } else {
+    return state
+  }
 }
